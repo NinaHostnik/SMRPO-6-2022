733e7d70e2bec84c7b2f90301c0af71f3570f7c5
NinaHostnik <nh7817@student.uni-lj.si>
2022-03-10 17:43:11 +0100

    beginning frameword

---
+++ app/Views/navbar.php
@@ -0,0 +1,10 @@
+<?= $this->extend('frame') ?>
+
+<?= $this->section('navbar') ?>
+    <nav class="navbar bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><b>SMRPO</b></a>
+            <button class="btn">Log-in</button>
+        </div>
+    </nav>
+<?= $this->endSection() ?>
