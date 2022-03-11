8c8559b6fd7bf7d8968428a9c5205e0d3e3b9990
JureTic <tic.jur@gmail.com>
2022-03-11 10:56:09 +0100

    merge

--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-<div class="topnav">
-    <a class="active" href="#">Home</a>
-    <a href="#">Sth</a>
-    <a href="#">Sth else</a>
-    <a href="#">More</a>
-</div>
-=======
-
 <nav class="navbar bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="#"><b>SMRPO</b></a>
         <button class="btn">Log-in</button>
     </div>
 </nav>
-
-
->>>>>>> f36324893fa15e346a755d96b9323c5ee9ce36e0
