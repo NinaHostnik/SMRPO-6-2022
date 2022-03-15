fe14d277c1535f18cafb8be965572918b93a8337
Nina Hostnik <nh7817@student.uni-lj.si>
2022-03-15 20:27:55 +0100

    adding goddamn mb-3 tag

--- app/Views/partials/formInput.php
+++ app/Views/partials/formInput.php
@@ -1,4 +1,4 @@
-<div class="form-group">
+<div class="form-group mb-3">
 
 <label for="<?php echo $id ?>"><?php echo $label ?></label>
 <input type="<?php echo $type ?>" class="form-control" name="<?php echo $id ?>" id="<?php echo $id ?>" value="<?php echo $value ?>">
