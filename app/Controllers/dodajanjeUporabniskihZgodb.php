9267d5208f8881593a4b639d6520b720dd680759
Benjamin Plut <benjamin.plut@hotmail.com>
2022-03-19 14:01:08 +0100

    napredek pri pridobivanju podatkov iz dodajanje zgodb

--- app/Controllers/dodajanjeUporabniskihZgodb.php
+++ app/Controllers/dodajanjeUporabniskihZgodb.php
@@ -10,6 +10,26 @@ class DodajanjeUporabniskihZgodb extends BaseController
         echo view('subpages/dodajanjeUporabniskihZgodb/dodajanjeUporabniskihZgodb');
     }
     public function dodajanjeZgodbe(){
-        echo view('subpages/dodajanjeUporabniskihZgodb/dodajanjeUporabniskihZgodb');
-    }
+
+        if ($this->request->getMethod() == 'post') {
+            $ime=$this->request->getVar('zgodbaIme');
+            $besedilo=$this->request->getVar('zgodbaBesedilo');
+            $prioriteta=$this->request->getVar('prioriteta');
+            $sprejemniTesti=$this->request->getVar('sprejemniTesti');
+            $poslovnaVrednost=$this->request->getVar('poslovnaVrednost');
+            $zgodba=[
+                'ime' => $ime,
+                'besedilo' => $besedilo,
+                'prioriteta' => $prioriteta,
+                'sprejemniTesti' => $sprejemniTesti,
+                'poslovnaVrednost' => $poslovnaVrednost,
+            ];
+            echo(json_encode($zgodba));
+        } else {
+            $data = [];
+
+            echo view('subpages/dodajanjeUporabniskihZgodb/dodajanjeUporabniskihZgodb');
+
+        }
+    }       
 }
