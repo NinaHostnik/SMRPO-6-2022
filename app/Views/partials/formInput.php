e236f3f3deadc6b7f69ed127cb5feac0a9248358
NinaHostnik <nh7817@student.uni-lj.si>
2022-03-24 00:08:53 +0100

    made formInput and passwordInput required fields (may be subject to change)

--- app/Views/partials/formInput.php
+++ app/Views/partials/formInput.php
@@ -1,4 +1,4 @@
 <div class="form-outline mb-4">
-    <input type="<?php echo $type ?>" class="form-control" name="<?php echo $id ?>" id="<?php echo $id ?>" value="<?php echo $value ?>">
+    <input type="<?php echo $type ?>" class="form-control" name="<?php echo $id ?>" id="<?php echo $id ?>" value="<?php echo $value ?>" required>
     <label for="<?php echo $id ?>"><?php echo $label ?></label>
 </div>
--- app/Views/partials/passwordInput.php
+++ app/Views/partials/passwordInput.php
@@ -12,7 +12,7 @@ function togglepass() {
 
 
 <div class="form-outline mb-4">
-    <input type="<?php echo $type ?>" class="form-control" name="<?php echo $id ?>" id="<?php echo $id ?>" value="<?php echo $value ?>">
+    <input type="<?php echo $type ?>" class="form-control" name="<?php echo $id ?>" id="<?php echo $id ?>" value="<?php echo $value ?>" required>
     <label for="<?php echo $id ?>"><?php echo $label ?></label>
     <!-- An element to toggle between password visibility -->
     <div class="float-end">
