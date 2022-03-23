17973e7fa2f94499b5961f79ff492c19d0672ac4
JureTic <tic.jur@gmail.com>
2022-03-23 17:04:48 +0100

    g

--- app/Config/App.php
+++ app/Config/App.php
@@ -55,7 +55,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $uriProtocol = 'QUERY_STRING';
+    public $uriProtocol = 'PATH_INFO';
 
     /**
      * --------------------------------------------------------------------------
