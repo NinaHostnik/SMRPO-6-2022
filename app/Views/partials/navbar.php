7da96441cbf19b4e4656890d459c269d8875468d
NinaHostnik <nh7817@student.uni-lj.si>
2022-03-24 20:22:32 +0100

    added cardTable with empty table

--- app/Views/partials/navbar.php
+++ app/Views/partials/navbar.php
@@ -1,11 +1,11 @@
 
 <nav class="navbar navbar-dark bg-dark fixed-top">
     <div class="container-fluid">
-        <a class="navbar-brand" href="HomeController::home"><b>SMRPO</b></a>
+        <a class="navbar-brand" href="/projekti"><b>SMRPO</b></a>
         <ul class="nav nav-pills nav-justify-content-end">
             <li class="nav-item px-2">
-                <form class="d-flex float-end" action="UserController::update_user">
-                    <a href="/profile" class="btn btn-outline-light" role="button"><?php echo session()->get('username')?></a>
+                <form class="d-flex float-end" action="UsersController::createUser">
+                    <a href="/admin/createUser" class="btn btn-outline-light" role="button">Ustvari novega uporabnika</a>
                 </form>
             </li>
             <li class="nav-item px-2">
@@ -14,6 +14,5 @@
                 </form>
             </li>
         </ul>
-
     </div>
 </nav>
