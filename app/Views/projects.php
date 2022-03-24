6ffe80a14ce853c1bc27c4b1768cd29af7ea4228
JureTic <tic.jur@gmail.com>
2022-03-24 22:53:07 +0100

    m

--- app/Views/projects.php
+++ app/Views/projects.php
@@ -6,7 +6,9 @@
 <h1>Projekti</h1>
 Projekti, ki se trenutno izvajajo:
 <hr>
+
 <?php
+
 foreach ($projekti as $projekt) {
 
  echo view("partials/projectCard", ['project_name' => $projekt['ime'], "project_desc" => $projekt['opis'],"project_id" => $projekt['id']]);
