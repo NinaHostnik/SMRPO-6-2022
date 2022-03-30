d0a59a52926a61d653eb990428902dc70e155260
NinaHostnik <nh7817@student.uni-lj.si>
2022-03-30 14:25:24 +0200

    Added all necessary fields to userCreate

--- app/Views/subpages/ustvarjanjeUporabnika/userCreate.php
+++ app/Views/subpages/ustvarjanjeUporabnika/userCreate.php
@@ -7,7 +7,11 @@
         <h5>Dodaj uporabnika</h5>
         <hr>
         <!-- Name input -->
+        <?php echo view("partials/formInput",['label'=>'Ime', "id"=>"name", 'type'=>'text', 'value'=>''])?>
+        <?php echo view("partials/formInput",['label'=>'Priimek', "id"=>"surname", 'type'=>'text', 'value'=>''])?>
+        <!-- Username input -->
         <?php echo view("partials/formInput",['label'=>'Uporabniško ime', "id"=>"username", 'type'=>'text', 'value'=>''])?>
+        <?php echo view('partials/formInput', ['label'=>'Email', 'id'=>'mail', 'type'=>'text', 'value'=>''])?>
         <!-- Permissions input -->
         <div class="form-group mb-3">
             <label for="permissions">Dovoljenja</label>
@@ -15,8 +19,10 @@
                 <option value="0">Administrator</option>
                 <option value="1">Uporabnik</option>
             </select>
-        </div>        <!-- Password input -->
+        </div>
+        <!-- Password input -->
         <?php echo view("partials/passwordInput",['label'=>'Geslo', "id"=>"password", 'type'=>'password','value'=>''])?>
+        <?//php echo view('partials/passwordInput', ['label'=>'Ponovi geslo', 'id'=>'passwordCheck', 'type'=>'password', 'value'=>''])?>
         <!-- Submit button -->
         <?php echo view('partials/formButton', ['name'=>'Dodaj Uporabnika']) ?>
     </form>
