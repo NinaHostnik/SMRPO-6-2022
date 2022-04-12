422339410a33cc0eddce72ece4858ba9f1805ecb
JureTic <tic.jur@gmail.com>
2022-04-12 21:39:17 +0200

    projectcard

--- app/Views/partials/projectCard.php
+++ app/Views/partials/projectCard.php
@@ -1,4 +1,5 @@
 <div class="row project_card" id="projectCard" onclick="location.href = '/cardTable/' + <?php echo $project_id ?>;">
     <h3><?php echo $project_name ?></h3>
     <p><?php echo $project_desc ?></p>
+    <p><?php echo session()->get('roles')[$project_id]?></p>
 </div>
