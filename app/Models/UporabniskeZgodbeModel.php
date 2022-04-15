f4645752307e319ea55a0cf2f46d84bfdb3121c4
JureTic <tic.jur@gmail.com>
2022-04-15 09:35:46 +0200

    vardumpery

--- app/Models/UporabniskeZgodbeModel.php
+++ app/Models/UporabniskeZgodbeModel.php
@@ -64,7 +64,6 @@ class UporabniskeZgodbeModel extends Model{
 
     function pridobiZgodbeSprinta($idSprinta){
         $query = $this->db-> query("SELECT * from uporabniskeZgodbe WHERE sprint = ".$idSprinta);
-        var_dump($query);
         return $query->getResultArray();
     }
 }
