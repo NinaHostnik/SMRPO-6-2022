760af2ca304c538a5746556f513684da196d1b1e
NinaHostnik <nh7817@student.uni-lj.si>
2022-04-21 14:53:28 +0200

    safety commit

--- app/Controllers/MyTasksController.php
+++ app/Controllers/MyTasksController.php
@@ -17,6 +17,9 @@ class MyTasksController extends BaseController
         $naloge = $nalogeModel->pridobiMojeNaloge($userID, $projectID);
         $zgodbe = $zgodbeModel->pridobiZgodbe(session()->get("projectId"));
 
+        $stories = $this->addTasksToStory($zgodbeNalog, $naloge);
+        #var_dump($stories);
+
 
         $zgodberework = $this->pridobizgodbe($zgodbe);
         $data = [
@@ -24,5 +27,18 @@ class MyTasksController extends BaseController
         ];
 
         echo view('subpages/projekti/myTasks',$data);
+    }
+
+    function addTasksToStory($zgodbeNalog, $naloge) {
+        foreach ($zgodbeNalog as $zgodba):
+            $zgodba['naloge'] = array();
+            $temp = array();
+            foreach ($naloge as $naloga):
+                if ($zgodba['idZgodbe'] === $naloga['zgodba_id']) $temp += $naloga;
+            endforeach;
+            $zgodba['naloge'] += $temp;
+        endforeach;
+        var_dump($zgodbeNalog);
+        return $zgodbeNalog;
     }
 }
