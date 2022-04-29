f18afc1e86f611a3511a8718b83debb4e1604142
BeniPlut1 <76884887+BeniPlut1@users.noreply.github.com>
2022-04-29 02:21:08 +0200

    Zakomentiral par vardump

--- app/Controllers/ProjectsController.php
+++ app/Controllers/ProjectsController.php
@@ -32,10 +32,10 @@ class ProjectsController extends BaseController
         $userModel = new UserModel();
         $zgodbe = $model->pridobiZgodbe(session()->get("projectId"));
         $fin = $this->addResponsibleAdults($zgodbe, $userModel);
-        var_dump($fin);
+        #var_dump($fin);
         echo "<br>";
         $zgodberework = $this->pridobizgodbe($fin);
-        var_dump($zgodberework);
+        #var_dump($zgodberework);
 
         $data = [
             'zgodbe'=>$zgodberework,
